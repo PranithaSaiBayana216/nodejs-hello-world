7c787317c0cc80ce9f180f6206ef7d81275627aa
PranithaSaiBayana216 <78683037+PranithaSaiBayana216@users.noreply.github.com>
2021-02-22 16:28:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello !!!!!! Welcome to nodejs application!!!");
+     response.end("Hiii !!!!!! Welcome to nodejs application second commit!!!");
 
 });
 
